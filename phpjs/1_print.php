f06cf99f503573f84923ab18538c26e1ff30214a
mong-head <camara1melon315@gmail.com>
2020-02-26 03:14:57 +0900

    HTML, CSS를 다하고, php,JavaScript를 하는 도중 commit

---
+++ phpjs/1_print.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <!-- php의 문법 -->
+    <h1>php</h1>
+    <?php
+    //php의 출력 : echo
+     echo "Hello world\n";
+     echo 10+10;
+    ?>
+
+    <!-- JavaScript의 문법 -->
+    <h1>JavaScript</h1>
+    <script type="text/javascript">
+    // js의 출력 : document.write()
+      document.write("Hello world\n");
+      document.write(10+10);
+    </script>
+  </body>
+</html>
